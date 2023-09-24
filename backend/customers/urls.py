d69ac2a6d73bbfbb9d3a5711255587704d4529ea
kerembay9 <kerembay9@gmail.com>
2023-09-24 19:42:34 +0300

    removed backend to recreate

--- backend/customers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.CustomerListCreateAPIView.as_view(),name="customer-list"),
-    path('<int:pk>/update/', views.CustomerUpdateAPIView.as_view(), name="customer-edit"),
-    path('<int:pk>/delete/', views.CustomerDestroyAPIView.as_view()),
-    path('<int:pk>/', views.CustomerDetailAPIView.as_view(), name="customer-detail"),
-]
